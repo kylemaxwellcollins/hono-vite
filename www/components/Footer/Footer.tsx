f2261e472ddc48d858c921fa02453262f2405c50
Kyle Collins <kyle.collins@zulubot.com>
2024-01-10 11:38:55 -0500

    Updated readme with environment variable fields

--- www/components/Footer/Footer.tsx
+++ www/components/Footer/Footer.tsx
@@ -2,6 +2,10 @@ const Footer = () => {
   return (
     <footer class="footer">
       <p>Footer</p>
+      <p>
+        Powered by:
+        <img class="footer__logo" src="assets/images/hono.svg" alt="" />
+      </p>
     </footer>
   );
 };
